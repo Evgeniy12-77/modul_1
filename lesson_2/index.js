cc7b6c89d6e0c3043d3475a0d635851fd93dcbe7
Евгений Толкач <tolkachevgeniy007@gmail.com>
2022-10-20 16:37:36 +0300

    изменили index.js

--- lesson_2/index.js
+++ lesson_2/index.js
@@ -1,20 +1,18 @@
 'use strict'
-{
+
    const productName = 'ноутбук';
    const productCategory = 'компьютерная техника';
    const quantityOfGoods = 25;
    const productPrice = 1000;
    console.log (productName);
    console.log (quantityOfGoods * productPrice);
-};
 
-{
+
    const item = 'Плед';
    const category = 'Постельные принадлежности';
    const quantity = 135;
    const price = 70;
    console.log (item);
    console.log (quantity * price);
-};
 
 
