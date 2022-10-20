f1170c94641331f5f2bf692bd007f00bd1ab6f6a
Евгений Толкач <tolkachevgeniy007@gmail.com>
2022-10-20 15:52:10 +0300

    initial

---
+++ lesson_2/index.js
@@ -0,0 +1,8 @@
+'use strict'
+
+   const productName = 'ноутбук';
+   const productCategory = 'компьютерная техника';
+   const quantityOfGoods = 25;
+   const productPrice = 1000;
+   console.log (productName);
+   console.log (quantityOfGoods * productPrice);
