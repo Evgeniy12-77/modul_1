c30b181609858f8d8da3f4162ea2dab5d0ace22e
Евгений Толкач <tolkachevgeniy007@gmail.com>
2022-10-20 20:00:01 +0300

    исправлены ошибки в index.js

--- lesson_2/index.js
+++ lesson_2/index.js
@@ -1,18 +1,19 @@
 'use strict'
-
+{
    const productName = 'ноутбук';
    const productCategory = 'компьютерная техника';
    const quantityOfGoods = 25;
    const productPrice = 1000;
    console.log (productName);
    console.log (quantityOfGoods * productPrice);
+};
 
-
-   const item = 'Плед';
-   const category = 'Постельные принадлежности';
-   const quantity = 135;
-   const price = 70;
+{
+   const productName = 'Плед';
+   const productCategory = 'Постельные принадлежности';
+   const quantityOfGoods = 135;
+   const productPrice = 70;
    console.log (item);
    console.log (quantity * price);
-
+}
 
