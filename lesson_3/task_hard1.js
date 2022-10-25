be650c7bf51a595c7991ed533b9a33f1ee4ccab0
Евгений Толкач <tolkachevgeniy007@gmail.com>
2022-10-25 16:41:00 +0300

    Исправили ошибки в 4-м задании

--- lesson_3/task_hard1.js
+++ lesson_3/task_hard1.js
@@ -3,12 +3,13 @@
 
 {
    const nalog = Number(prompt('Введите сумму вашего дохода'));
-   nalog > 0;
+   
    const nalog1 = nalog * 0.13;
    const nalog2 = nalog * 0.20;
    const nalog3 = nalog * 0.30;
 
-   if (nalog <= 15000) {
+   
+   if (nalog > 0 && nalog <= 15000) {
       console.log (nalog1);
    } else if (nalog > 15000 && nalog <= 50000) {
       console.log (nalog2); 
--- lesson_3/task_hard2.js
+++ lesson_3/task_hard2.js
@@ -8,7 +8,7 @@ const nalog1 = nalog * 0.13;
 const nalog2 = (nalog - 15000) * 0.20;
 const nalog3 = (nalog - 50000) * 0.30;
 
-if (nalog <= 15000) {
+if (nalog > 0 && nalog <= 15000) {
    console.log (nalog1);
 } else if (nalog > 15000 && nalog <= 50000) {
    console.log (nalog2); 
