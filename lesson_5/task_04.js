788724b3b70539b88566e16570698bb0f74f9142
Евгений Толкач <tolkachevgeniy007@gmail.com>
2022-10-29 22:51:00 +0300

    Подредактировал файл task_04

--- lesson_5/task_04.js
+++ lesson_5/task_04.js
@@ -3,29 +3,24 @@
 // Функция с условиями
 
 function calculate(sumGoods, numberGoods, promo = null) {
-   let sum = sumGoods;
+   let sum_12 = sumGoods;
 
-   // Первое условие, если сумма товаров 10 или больше 10
    if (numberGoods >= 10) {
-      const countThreeProcent = sum * 0.03;
-      console.log('больше 10 товаров ваша скидка ' + countThreeProcent);
-   }
-   // Второе условие, если сумма товаров превышает 30000
-   if (sumGoods > 30000) {
-      const difFerence = sum - 30000;
-      const countFifteenProcent = difFerence * 0.15;      
-      console.log('Больше 30000 ваша скидка ' + countFifteenProcent);
-   }
-   // Третье условие, если есть промокод
-   if (promo === 'METHED') {
-      const countTenProcent = sum * 0.10;
-      console.log('Ваша скидка по промокоду ' + countTenProcent);
-   }
-   // Четвертое условие, если есть промокод и если сумма скидок больше 2000
-   if (promo === 'G3H2Z1' && ((countThreeProcent + countFifteenProcent + countTenProcent) > 2000)) {
-      const promo_500 = sum - 500;
-      console.log('Ваша дополнительная скидка 500');
-   }
+   const threeProcent = sum_12 * 0.03;
+   console.log("Ваша скидка составила " + threeProcent);
+} 
+if (sumGoods > 30000) {
+   const fifteenProcent = (sum_12 - 30000) * 0.15;
+   console.log("Ваша скидка составила " + fifteenProcent);
+} 
+if (promo === 'METHED') {
+   const tenProcent = sum_12 * 0.10; 
+   console.log("Ваша скидка составила " + tenProcent);
+ }
+ sumGoods -= sum_12 - (sum_12 * 0.03) - ((sum_12 - 30000) * 0.15) - (sum_12 * 0.10);
+ if (promo === 'G3H2Z1' && sumGoods > 2000) {
+  sum_12 -= sum_12 - 500;
+   console.log('Ваша дополнительная скидка составила ' + 500);
+ }
 };
-
-calculate(100000, 30, 'METHED');
+calculate(31000, 30, 'G3H2Z1');
