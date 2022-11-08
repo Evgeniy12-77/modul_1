18d1d4c0acbcbed657988d48918d2197c62d17a0
Евгений Толкач <tolkachevgeniy007@gmail.com>
2022-11-08 17:34:25 +0300

    Сделал 8.1, 8.2

---
+++ lesson_8/task_01.js
@@ -0,0 +1,19 @@
+'use strict'
+
+
+
+
+const fooR = (length) => {
+   const newArrr = [];
+   const arrLengts = length;
+
+   for (let i = 1; i >= 1, i <= arrLengts; i++) {
+   const newNewArr = Math.floor(Math.random() * 100);
+   newArrr.push(newNewArr);
+} return newArrr;
+};
+console.log(fooR(100));
+   
+   
+   
+
---
+++ lesson_8/task_02.js
@@ -0,0 +1,15 @@
+
+
+const fooR = (length, n, m) => {
+   const newArrr = [];
+   arrLengts = length;
+   const minNum = n;
+   const maxNum = m;
+   for (let i = 1; i > 0, i < arrLengts; i++) {
+   const newNewArr = Math.floor(Math.random() * (m - n) + n);
+   newArrr.push(newNewArr);
+} return newArrr;
+};
+console.log(fooR(10, -1000, 10000));
+
+
