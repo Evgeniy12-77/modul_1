9bc9021f531e3d1d37a6f207bc318130bb0c2141
Евгений Толкач <tolkachevgeniy007@gmail.com>
2023-02-08 16:26:37 +0300

    сделал 1-е задание модуль 2

--- MODUL_2/lesson_1/index.js
+++ MODUL_2/lesson_1/index.js
@@ -4,11 +4,31 @@ const ads = document.querySelector('.ads');
 ads.remove();
 
 const itemDom = document.querySelectorAll('.item');
+const itemListParent = document.querySelector('.items');
+itemDom[0].replaceWith(itemDom[1]);
+itemDom[4].replaceWith(itemDom[0]);
+itemDom[5].replaceWith(itemDom[4]);
+itemListParent.insertBefore(itemDom[5], null);
 
 console.log(itemDom);
 
-//itemDom[1].replaceWith(itemDom[2]);
-//itemDom[2].replaceWith(itemDom[1]);
+const propsListDom = document.querySelectorAll('.props__list');
+console.log(propsListDom);
+
+propsListDom[2].replaceWith(propsListDom[4]);
+itemDom[4].append(propsListDom[2]);
+
+const propsEs6 = document.querySelectorAll('.props__item_two');
+
+console.log(propsEs6);
+
+propsEs6[7].append(propsEs6[8]);
+propsEs6[7].append(propsEs6[9]);
+
+const propsJS = document.querySelectorAll('.props__item_four');
+console.log(propsJS);
+propsJS[3].append(propsJS[0]);
+
 
 
 
